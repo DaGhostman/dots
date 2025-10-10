10dee33a8b35b46aad35a4f3a2942ad5db08f976
Dimitar Dimitrov <daghostman.dd@gmail.com>
2025-10-10 15:48:16 +0300

    add buffer handling that does not mess with window layout

--- nvim/lua/core/tabs.lua
+++ nvim/lua/core/tabs.lua
@@ -1,21 +1,3 @@
 return {
-    {
-        "romgrk/barbar.nvim",
-        dependencies = {
-            "nvim-tree/nvim-web-devicons",
-        },
-        config = function()
-            require('barbar').setup {
-                animation = false,
-                tabpages = false,
-                clickable = false,
-                icons = {
-                    filetype = {
-                        enabled = false,
-                    }
-                },
-                auto_hide = 1,
-            }
-        end
-    },
+    { "N8WM/buffexit.nvim" },
 }
