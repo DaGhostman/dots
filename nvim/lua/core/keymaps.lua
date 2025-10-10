9f67b7826b8c66034084b1ddf571b68124b3aa8d
Dimitar Dimitrov <daghostman.dd@gmail.com>
2025-10-10 16:13:45 +0300

    Reworked the mappings to make them more intuitive

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -1,22 +1,26 @@
+local function nmap(sequence, command, description)
+    vim.keymap.set('n', sequence, command, { silent = true, desc = description })
+end
+
 vim.keymap.set('n', '<Esc>', vim.cmd.nohlsearch, { silent = true })
 
-vim.keymap.set('n', 'bd', function() require("buffexit").bdelete() end, { silent = true })
-
-vim.keymap.set('n', '<A-,>', function() require('barbar').previous() end, { silent = true })
-vim.keymap.set('n', '<A-.>', function() require('barbar').next() end, { silent = true })
+nmap('dcb', function() require("buffexit").bdelete() end, "[D]elete the [C]urrent [B]uffer")
 
 -- // QUICKFIX NAV
-vim.keymap.set('n', '<C-j>', vim.cmd.cnext, { silent = true })
-vim.keymap.set('n', '<C-k>', vim.cmd.cprev, { silent = true })
+nmap('<C-j>', vim.cmd.cnext, "Quickfix next")
+nmap('<C-k>', vim.cmd.cprev, "Qucikfix prev")
 
 -- // NAVIGATION
-vim.keymap.set('n', 'grr', function() vim.lsp.buf.references() end, { silent = true })
-vim.keymap.set('n', 'grd', function() vim.lsp.buf.definition() end, { silent = true })
-vim.keymap.set('n', 'gri', function() vim.lsp.buf.implementation() end, { silent = true })
-vim.keymap.set('n', 'gra', function() vim.lsp.buf.code_action() end, { silent = true })
-vim.keymap.set('n', 'grn', function() vim.lsp.buf.rename() end, { silent = true })
-vim.keymap.set('n', 'grb', function() require("fzf-lua").buffers() end, { silent = true })
-vim.keymap.set('n', 'grf', function() require("fzf-lua").files() end, { silent = true })
-vim.keymap.set('n', 'grg', function() require("fzf-lua").grep() end, { silent = true })
+nmap('grr', function() vim.lsp.buf.references() end, "[G]et [R]elative [R]eferences")
+nmap('grd', function() vim.lsp.buf.definition() end, "[G]et [R]elative [D]efinition")
+nmap('gri', function() vim.lsp.buf.implementation() end, "[G]et [R]elative [I]mplementation")
+nmap('gra', function() vim.lsp.buf.code_action() end, "[G]et [R]elative [A]ction")
+nmap('grn', function() vim.lsp.buf.rename() end, "[G]et [R]elative re[N]ame")
+nmap('gds', function() vim.lsp.buf.document_symbol() end, "[G]ets [D]ocument [S]ymbols")
+
+nmap('<Leader>b', function() require("fzf-lua").buffers() end, "[B]uffers")
+nmap('<Leader>f', function() require("fzf-lua").files() end, "[F]iles")
+nmap('<C-f>', function() require("fzf-lua").files() end, "Alias of <Leader>f")
+nmap('<Leader>g', function() require("fzf-lua").grep() end, "[G]rep")
 
 return {}
