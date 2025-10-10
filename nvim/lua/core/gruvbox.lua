b24b2057f225d9cf9e92462379f9a9703f3fde50
Dimitar Dimitrov <daghostman.dd@gmail.com>
2025-10-10 15:48:37 +0300

    format and remove dimming

--- nvim/lua/core/gruvbox.lua
+++ nvim/lua/core/gruvbox.lua
@@ -1,9 +1,11 @@
 return {
     {
-        "ellisonleao/gruvbox.nvim", priority = 1000, config = function()
+        "ellisonleao/gruvbox.nvim",
+        priority = 1000,
+        config = function()
             require("gruvbox").setup {
                 transparent_mode = true,
-                dim_inactive = true,
+                dim_inactive = false,
                 invert_tabline = true,
             }
 
